bcd0eebcbe5a388a74125128dcb01e2cde4e0cd5
Stefano <stefano.marzorati@gmail.com>
2019-08-25 14:52:29 +0200

    Added new post

---
+++ sw.js
@@ -0,0 +1,9 @@
+<script type="application/javascript" src="https://sdk.truepush.com/sdk/v2/app.js"></script>
+<script>
+var truepush = window.truepush || [];
+truepush.push(function(){
+truepush.Init({
+id: "5d6282fbc5dc797140fac4f3"
+})
+})
+</script>  
